b76344764d1d66933acfcc4e22082ef483727b47
Constantin <kostiantynkalinin@gmail.com>
2016-02-29 16:46:36 +0000

    Implemented functionality of adding new restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -5,7 +5,15 @@ class RestaurantsController < ApplicationController
   end
 
   def new
-    
+
   end
 
+  def create
+    Restaurant.create(restaurant_params)
+    redirect_to '/restaurants'
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name)
+  end
 end
